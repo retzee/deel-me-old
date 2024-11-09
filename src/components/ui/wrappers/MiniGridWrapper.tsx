d7b56648ccfa1d61630818dafa96c0a4b63e70a6
Sam Odele <odelesamson@gmail.com>
2024-11-09 21:03:17 +0100

    Update

--- src/components/ui/wrappers/MiniGridWrapper.tsx
+++ src/components/ui/wrappers/MiniGridWrapper.tsx
@@ -2,8 +2,8 @@ import { PropsWithChildren } from 'react'
 
 const MiniGridWrapper = (props: PropsWithChildren) => {
     return (
-        <div class="grid grid-cols-6 gap-4 mb-8">
-            <div class="col-start-3 col-span-2 border-2 border-red-800 text-center rounded-lg shadow">
+        <div className="grid grid-cols-6 gap-4 mb-8">
+            <div className="col-start-3 col-span-2 border-2 border-red-800 text-center rounded-lg shadow">
                 <div className="p-6 space-y-4 md:space-y-6 sm:p-8">
                     {props.children}
                 </div>
